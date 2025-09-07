79c741150a7d2e3f829892d34c7cff8e366bcc4b
snskar <s4nskar@gmail.com>
2025-09-07 19:49:37 +0530

    fix admin page bg

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,10 @@ import type { ReactNode } from 'react';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="text-gray-900 antialiased" style={{ colorScheme: 'light' }}>
+    <div
+      className="text-gray-900 antialiased bg-white"
+      style={{ colorScheme: 'light' }}
+    >
       {children}
     </div>
   );
